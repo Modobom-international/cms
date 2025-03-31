601194b03a533c3e683bae2e179ca107c23b861c
binhchay1 <binhchay1@gmail.com>
2025-03-31 03:18:34 +0000

    fix controller

--- routes/api.php
+++ routes/api.php
@@ -18,34 +18,83 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/me', [UserController::class, 'me']);
     Route::post('/update/user', [UserController::class, 'updateCurrentUser']);
     Route::post('/change-password', [UserController::class, 'changePassword']);
-    
+
     //admin change password for useer
     Route::post('/change-password-user/{id}/', [UserController::class, 'updatePassword']);
-    
+
     //workspace
     Route::get('/workspaces', [WorkspaceController::class, 'index']);
     Route::post('/create-workspace', [WorkspaceController::class, 'store']);
     Route::get('/workspace/{id}', [WorkspaceController::class, 'show']);
     Route::post('/update-workspace/{id}', [WorkspaceController::class, 'update']);
     Route::get('/delete-workspace/{id}', [WorkspaceController::class, 'destroy']);
-    
+
     //workspace-user
     Route::post('/workspace/{id}/join', [WorkspaceController::class, 'joinPublicWorkspace']);
     Route::post('/workspace/{id}/add-member', [WorkspaceController::class, 'addMember']);
     Route::post('/workspace/remove-members', [WorkspaceController::class, 'removeMember']);
     Route::get('/workspace/{id}/members', [WorkspaceController::class, 'listMembers']);
-    
+
     //board
     Route::post('/create-board', [BoardController::class, 'store']); // Tạo Board
     Route::get('/board/{id}', [BoardController::class, 'show']);
     Route::get('/workspace/{id}/boards', [BoardController::class, 'index']); // Lấy danh sách Board
     Route::post('/update-board/{id}', [BoardController::class, 'update']); // Cập nhật Board
     Route::delete('/delete-board/{id}', [BoardController::class, 'destroy']); // Xóa Board
-    
+
     //board-user
     Route::post('/board/{id}/join', [BoardController::class, 'joinPublicBoard']);
     Route::post('/board/{id}/add-member', [BoardController::class, 'addMember']);
     Route::post('/board/remove-members', [BoardController::class, 'removeMember']);
     Route::get('/board/{id}/members', [BoardController::class, 'listMembers']);
-    
+
+    Route::prefix('domain')->group(function () {
+        Route::get('/', [DomainController::class, 'listDomain'])->name('domain.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/create', [DomainController::class, 'createDomain'])->name('domain.create')->middleware(ShareGlobalVariable::class);
+        Route::get('/check', [DomainController::class, 'checkDomain'])->name('domain.check');
+        Route::get('/up', [DomainController::class, 'upDomain'])->name('domain.up');
+        Route::get('/search', [DomainController::class, 'searchDomain'])->name('domain.search');
+        Route::get('/delete', [DomainController::class, 'deleteDomain'])->name('domain.delete');
+    });
+
+    Route::prefix('html-source')->group(function () {
+        Route::get('/', [HtmlSourceController::class, 'listHtmlSource'])->name('html.source.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/{id}', [HtmlSourceController::class, 'showHtmlSource'])->name('html.source.show');
+    });
+
+    Route::prefix('users-tracking')->group(function () {
+        Route::get('/', [UsersTrackingController::class, 'viewUsersTracking'])->name('users.tracking.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/get-detail-tracking', [UsersTrackingController::class, 'getDetailTracking'])->name('users.tracking.detail');
+        Route::get('/get-heat-map', [UsersTrackingController::class, 'getHeatMap'])->name('users.tracking.heat.map');
+        Route::get('/get-link-for-heat-map', [UsersTrackingController::class, 'getLinkForHeatMap'])->name('users.tracking.link.heat.map');
+    });
+
+    Route::prefix('team')->group(function () {
+        Route::get('/', [TeamController::class, 'index'])->name('team.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/create', [TeamController::class, 'create'])->name('team.create')->middleware(ShareGlobalVariable::class);
+        Route::post('/store', [TeamController::class, 'store'])->name('team.store');
+        Route::post('/update/{id}', [TeamController::class, 'update'])->name('team.update');
+        Route::get('/edit/{id}', [TeamController::class, 'edit'])->name('team.edit')->middleware(ShareGlobalVariable::class);
+        Route::get('/delete/{id}', [TeamController::class, 'destroy'])->name('team.delete');
+        Route::get('/get-permission-by-team', [TeamController::class, 'getPermissionByTeam'])->name('team.get.permission');
+    });
+
+    Route::prefix('push-system')->group(function () {
+        Route::get('/', [PushSystemController::class, 'listPushSystem'])->name('push.system.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/config-link/add', [PushSystemController::class, 'addConfigSystemLink'])->name('push.system.config.link');
+        Route::get('/list-user-active', [PushSystemController::class, 'listUserActiveAjax'])->name('push.system.list.user.active.ajax');
+        Route::get('/show-config-links', [PushSystemController::class, 'showConfigLinksPush'])->name('push.system.show.config.link');
+        Route::get('/config-links', [PushSystemController::class, 'configLinksPush'])->name('push.system.edit.config.link');
+    });
+
+    Route::prefix('log-behavior')->group(function () {
+        Route::get('/', [LogBehaviorController::class, 'viewLogBehavior'])->name('log.behavior.list')->middleware(ShareGlobalVariable::class);
+        Route::get('/get-data-chart', [LogBehaviorController::class, 'getDataChartLogBehavior'])->name('log.behavior.chart');
+        Route::get('/store-config-filter', [LogBehaviorController::class, 'storeConfigFilterLogBehavior'])->name('log.behavior.store.config.filter');
+        Route::get('/reset-config-filter', [LogBehaviorController::class, 'resetConfigFilterLogBehavior'])->name('log.behavior.reset.config.filter');
+        Route::get('/compare-date', [LogBehaviorController::class, 'compareDate'])->name('log.behavior.compare.date');
+        Route::get('/save-list-app-for-check', [LogBehaviorController::class, 'saveListAppForCheck'])->name('log.behavior.save.app.in.checklist');
+        Route::get('/delete-app-in-list-for-check', [LogBehaviorController::class, 'deleteAppInListForCheck'])->name('log.behavior.delete.app.in.checklist');
+        Route::get('/get-activity-uid', [LogBehaviorController::class, 'getActivityUid'])->name('log.behavior.activity.uid');
+    });
 });
