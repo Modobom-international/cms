d6fd8064c8bee4862a72ae76460229ce0386b6d9
binhchay1 <binhchay1@gmail.com>
2025-04-01 05:38:36 +0000

    add users-tracking

---
+++ app/Http/Controllers/API/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function generateToken(Request $request)
+    {
+        $user = Auth::user();
+        if (!$user) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $token = Str::random(40);
+        $user->exclude_token = $token;
+        $user->save();
+
+        return response()->json(['exclude_token' => $token]);
+    }
+
+    public function verifyToken(Request $request)
+    {
+        $token = $request->input('token');
+        $user = User::where('exclude_token', $token)->first();
+
+        if ($user) {
+            return response()->json(['valid' => true]);
+        }
+
+        return response()->json(['valid' => false]);
+    }
+}
