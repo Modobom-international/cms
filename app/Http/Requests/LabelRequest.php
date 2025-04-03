7eb4e54128c951bed68ae3383a40092dc381fa4d
tranthuy98 <tranthuy240814@gmail.com>
2025-04-03 11:03:54 +0700

    featrue-labels

---
+++ app/Http/Requests/LabelRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LabelRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true; //
+    }
+    
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'color' => 'required|string|regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/',
+        ];
+    }
+    
+    public function messages()
+    {
+        return [
+            'name.required' => 'Tên label là bắt buộc.',
+            'color.regex' => 'Màu sắc phải là mã màu HEX hợp lệ.',
+        ];
+    }
+}
+
