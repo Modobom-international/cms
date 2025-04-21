b9f5ad46ab2a3a1316be7e067fca0644b33a5fa2
binhchay1 <binhchay1@gmail.com>
2025-04-21 08:22:09 +0000

    add log

--- app/Jobs/RefreshDomainInPlatform.php
+++ app/Jobs/RefreshDomainInPlatform.php
@@ -26,14 +26,14 @@ class RefreshDomainInPlatform implements ShouldQueue
             $result = $goDaddyService->getListDomain();
             $listDomain = array();
 
-            foreach ($result['data'] as $domain) {
-                $listDomain[] = $domain['domain'];
-            }
-
             if (array_key_exists('error', $result)) {
                 $this->messageEventHandler('Lỗi không lấy được danh sách domain từ Godaddy ...');
 
                 return $this->error($result['error']);
+            }
+
+            foreach ($result['data'] as $domain) {
+                $listDomain[] = $domain['domain'];
             }
 
             $listExist = $domainRepository->getDomainByList($listDomain);
@@ -49,6 +49,14 @@ class RefreshDomainInPlatform implements ShouldQueue
                         'jean.ns.cloudflare.com',
                     ];
 
+                    if (array_key_exists('renewDeadline', $infoDomain)) {
+                        $renewDeadline = Carbon::parse($infoDomain['renewDeadline'])->format('Y-m-d H:i:s');
+                    } else {
+                        $renewDeadline = null;
+                    }
+
+                    $arrayIndex[] = $index;
+
                     $data = [
                         'domain' => $domain,
                         'time_expired' => Carbon::parse($infoDomain['expires'])->format('Y-m-d H:i:s'),
@@ -57,7 +65,7 @@ class RefreshDomainInPlatform implements ShouldQueue
                         'renewable' => $infoDomain['renewable'] ?? false,
                         'status' => $infoDomain['status'] ?? 'active',
                         'name_servers' => json_encode($nameServer) ?? null,
-                        'renew_deadline' => Carbon::parse($infoDomain['renewDeadline'])->format('Y-m-d H:i:s') ?? null,
+                        'renew_deadline' => $renewDeadline,
                         'registrar_created_at' => Carbon::parse($infoDomain['registrarCreatedAt'])->format('Y-m-d H:i:s') ?? null,
                     ];
 
@@ -65,11 +73,11 @@ class RefreshDomainInPlatform implements ShouldQueue
 
                     $this->messageEventHandler('Thêm domain ' . $domain . ' vào hệ thống thành công ...');
 
-                    $result = $cloudFlareService->addDomain(
+                    $resultWithoutData = $cloudFlareService->addDomain(
                         $domain
                     );
 
-                    if (array_key_exists('error', $result)) {
+                    if (array_key_exists('error', $resultWithoutData)) {
                         $this->messageEventHandler('Thêm domain ' . $domain . ' vào Cloudflare không thành công  ...');
 
                         return;
@@ -77,11 +85,11 @@ class RefreshDomainInPlatform implements ShouldQueue
                         $this->messageEventHandler('Thêm domain ' . $domain . ' vào Cloudflare thành công ...');
                     }
 
-                    $result = $goDaddyService->updateNameservers(
+                    $resultWithoutData = $goDaddyService->updateNameservers(
                         $domain
                     );
 
-                    if (is_array($result) and array_key_exists('error', $result)) {
+                    if (is_array($resultWithoutData) and array_key_exists('error', $resultWithoutData)) {
                         $this->messageEventHandler('Sửa DNS ' . $domain . ' trên Godaddy không thành công  ...');
 
                         return;
@@ -95,8 +103,9 @@ class RefreshDomainInPlatform implements ShouldQueue
 
             $this->messageEventHandler('Hoàn thành kiểm tra domain! Bro có thể tắt hộp thoại này đi được rồi nhé');
         } catch (\Exception $e) {
-            $this->messageEventHandler('Xử lý đồng bộ domain lỗi. Vui lòng liên hệ bộ phận code!' . $e->getMessage());
-            Log::error('Error in RefreshDomainInPlatform: ' . $e->getMessage());
+            $stringError = ' , result[data] = ' . json_encode($result['data']) . ' , ' . json_encode($infoDomain);
+            $this->messageEventHandler('Xử lý đồng bộ domain lỗi. Vui lòng liên hệ bộ phận code!');
+            Log::error('Error in RefreshDomainInPlatform: ' . $e->getMessage() . $stringError);
         }
     }
 
