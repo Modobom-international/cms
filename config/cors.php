e12b3d88193f897b0b7647ddfd8ab94fbbde2644
binhchay1 <binhchay1@gmail.com>
2025-04-21 08:50:16 +0000

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
