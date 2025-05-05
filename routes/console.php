be08c0ca20ac7467f7325cf65125e5e9608c2e14
binhchay1 <binhchay1@gmail.com>
2025-05-05 09:04:19 +0000

    fix console

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,7 @@
 
 use App\Jobs\StoreServerStat;
 use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\Domain\SyncDomainForAccount;
 
 Schedule::job(new StoreServerStat)->everyFiveSeconds();
+Schedule::command(new SyncDomainForAccount)->everyFiveMinutes()->withoutOverlapping()->runInBackground();
