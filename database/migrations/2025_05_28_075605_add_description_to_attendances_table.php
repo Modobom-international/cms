3d6a29a572ecf06c54a5c76e82cccf51866f793f
dkmtech32 <dkmtien123@gmail.com>
2025-05-28 16:58:30 +0700

    Update migration to remove 'after' clause for 'description' column in attendances table

--- database/migrations/2025_05_28_075605_add_description_to_attendances_table.php
+++ database/migrations/2025_05_28_075605_add_description_to_attendances_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('attendances', function (Blueprint $table) {
-            $table->text('description')->nullable()->after('branch_name');
+            $table->text('description')->nullable();
         });
     }
 
