554a16611046a056085c4c8abca707143ae76cfd
binhchay1 <binhchay1@gmail.com>
2025-06-16 09:44:16 +0000

    change model

--- app/Models/AppInformation.php
+++ app/Models/AppInformation.php
@@ -8,7 +8,7 @@ class AppInformation extends Model
 {
     protected $connection = 'mongodb';
 
-    protected $collection = 'config_pools';
+    protected $collection = 'app_information';
 
     protected $fillable = ['request_id', 'app_name', 'app_id', 'app_version', 'os_name', 'os_version', 'event_name', 'event_value', 'category'];
 }
